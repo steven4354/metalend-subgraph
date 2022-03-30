7cf937a0236a30756e42a84be9e4db6067f8134b
steven4354 <steven4354@gmail.com>
2022-03-29 21:27:11 -0700

    delete borrow logic

--- metalend/src/cERC20.ts
+++ metalend/src/cERC20.ts
@@ -28,57 +28,7 @@ export function handleAccrueInterest(event: AccrueInterest): void {}
 
 export function handleApproval(event: Approval): void {}
 
-export function handleBorrow(event: Borrow): void {
-  let market = Market.load(event.address.toHexString())
-  let accountID = event.params.borrower.toHex()
-
-  // // Update cTokenStats common for all events, and return the stats to update unique
-  // // values for each event
-  // let cTokenStats = updateCommonCTokenStats(
-  //   market.id,
-  //   market.symbol,
-  //   accountID,
-  //   event.transaction.hash,
-  //   event.block.timestamp.toI32(),
-  //   event.block.number.toI32(),
-  // )
-
-  // let borrowAmountBD = event.params.borrowAmount
-  //   .toBigDecimal()
-  //   .div(exponentToBigDecimal(market.underlyingDecimals))
-  // let previousBorrow = cTokenStats.storedBorrowBalance
-
-  // cTokenStats.storedBorrowBalance = event.params.accountBorrows
-  //   .toBigDecimal()
-  //   .div(exponentToBigDecimal(market.underlyingDecimals))
-  //   .truncate(market.underlyingDecimals)
-
-  // cTokenStats.accountBorrowIndex = market.borrowIndex
-  // cTokenStats.totalUnderlyingBorrowed = cTokenStats.totalUnderlyingBorrowed.plus(
-  //   borrowAmountBD,
-  // )
-  // cTokenStats.save()
-
-  // let account = Account.load(accountID)
-  // if (account == null) {
-  //   account = createAccount(accountID)
-  // }
-  // account.hasBorrowed = true
-  // account.save()
-
-  // if (
-  //   previousBorrow.equals(zeroBD) &&
-  //   !event.params.accountBorrows.toBigDecimal().equals(zeroBD) // checking edge case for borrwing 0
-  // ) {
-  //   market.numberOfBorrowers = market.numberOfBorrowers + 1
-  //   market.save()
-  // }
-
-  if (market !== null) {
-    market.numberOfBorrowers = market.numberOfBorrowers + 1
-    market.save()
-  }
-}
+export function handleBorrow(event: Borrow): void {}
 
 export function handleFailure(event: Failure): void {}
 
